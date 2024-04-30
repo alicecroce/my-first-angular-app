86f5c77613ac9e8798d0a304782dfdf4c9260161
alicecroce <alicecroce2@gmail.com>
2024-04-30 11:19:11 +0200

    Create new folder services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,

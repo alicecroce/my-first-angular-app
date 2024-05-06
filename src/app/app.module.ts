abff006d2b2d9bf5fe45d17ea31f33830a4712b4
alicecroce <alicecroce2@gmail.com>
2024-05-06 09:01:04 +0200

    Ho creato con CLI la cartella success-alert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServicesComponent } from './services/services.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServicesComponent,
-    WarningAlertComponent
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,

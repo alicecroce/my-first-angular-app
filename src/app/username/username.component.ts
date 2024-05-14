a47cc5ab936553f71dad60a99653e81228d539e7
alicecroce <alicecroce2@gmail.com>
2024-05-14 11:12:59 +0200

    Creazione bottone per inserire Username e reset dello stesso

--- src/app/username/username.component.ts
+++ src/app/username/username.component.ts
@@ -1,10 +1,10 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-username',
     templateUrl: './username.component.html'
 })
 
-export class UsernameComponent {
-    
+export class UsernameComponent {   
+    username='';
 }

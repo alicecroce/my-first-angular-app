be049e95bcc9ed946909bb568ebcf425b0fad2f7
alicecroce <alicecroce2@gmail.com>
2024-05-21 17:35:07 +0200

    Peolessità da analizzare su ngStyle ed ngFor

--- src/app/display-details/display-details.component.ts
+++ src/app/display-details/display-details.component.ts
@@ -2,9 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-display-details',
-    templateUrl: './display-details.component.html'
+    templateUrl: './display-details.component.html',
+    styleUrls:['./display-details.component.css']
 })
 
 export class DisplayDetailsComponent {
-    showSecreat=false;
+    showSecret=false;
+    log=[];
+
+    onToggleDetails(){
+        this.showSecret=!this.showSecret;
+        this.log.push(this.log.length+1);
+    }
 }
